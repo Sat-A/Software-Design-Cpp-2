b8d08dc005807db81ff41dcc9c8ba8ed87d0739d
Satyam_A <thesatyam@outlook.com>
2025-02-22 22:47:24 +0000

    Update maps and initialise nodes

--- Q7/Q7.cpp
+++ Q7/Q7.cpp
@@ -1,22 +1,51 @@
-#include <cmath>
 #include <iostream>
-#include "Q7.hpp"
+#include <map>
+#include <vector>
+#include <string>
 using namespace std;
 
-Coord::Coord(Coord & C) : x(C.x), y(C.y) {}
-Coord::Coord(double x, double y) : x(x), y(y) {}
-double Coord::getX() const { return x; }
-double Coord::getY() const { return y; }
+class Map {
+public:
+    Map(const Map &M) : data(M.data) {}
+    Map(map<vector<char>, double> m) : data(m) {}
 
-double Distance(Coord & c1, Coord & c2) {
-    return sqrt(pow(c1.getX() - c2.getX(), 2) + pow(c1.getY() - c2.getY(), 2));
-}
+    void display() const {
+        for (const auto &pair : data) {
+            for (char c : pair.first) {
+                cout << c << " ";
+            }
+            cout << ": " << pair.second << endl;
+        }
+    }
 
-Map::Map(Map & M) {}
-Map::Map(map<Coord, vector<Coord>>) {}
+private:
+    map<vector<char>, double> data;
+};
 
 int main() {
-    Coord c1(2, 2);
-    Coord c2(3, 3);
-    cout << Distance(c1, c2) << endl;
-};
+    // Initialise Map
+    map<vector<char>, double> m = {
+        {{'s', 'a'}, 0.1},
+        {{'s', 'b'}, 0.2},
+        {{'s', 'c'}, 0.5},
+        {{'s', 'd'}, 1},
+        {{'s', 'e'}, 0.7},
+        {{'a', 'f'}, 0.2},
+        {{'f', 'g'}, 0.9},
+        {{'f', 'h'}, 0.4},
+        {{'e', 'i'}, 0.2},
+        {{'h', 'j'}, 0.8},
+        {{'a', 'b'}, 0.3},
+        {{'j', 'a'}, 0.5},
+        };
+    Map map1(m);
+    Map map2(map1);
+
+    cout << "Map 1:" << endl;
+    map1.display();
+
+    cout << "Map 2:" << endl;
+    map2.display();
+
+    return 0;
+}
